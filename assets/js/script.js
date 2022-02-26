aa09978a6044cf53f035cdf154cb1d8f62abc4c2
urrasinh <urrasinh@gmail.com>
2022-02-26 11:31:08 -0300

    agregando grafico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
-const pokemonNombre = document.querySelector("#pokemon-nombre");
-const pokemonTabla = document.querySelector("#pokemon-tabla");
-const pokemonImagen = document.querySelector("#pokemon-imagen");
-const pokemonFormulario = document.querySelector("#pokemon-formulario");
-const pokemonBuscar = document.querySelector("#pokemon-buscar");
+const pokemonNombre = document.querySelector("#pokemon-nombre")
+const pokemonTabla = document.querySelector("#pokemon-tabla")
+const pokemonImagen = document.querySelector("#pokemon-imagen")
+const pokemonFormulario = document.querySelector("#pokemon-formulario")
+const pokemonBuscar = document.querySelector("#pokemon-buscar")
 
 pokemonFormulario.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const pokemon = pokemonBuscar.value;
+    event.preventDefault()
+    const pokemon = pokemonBuscar.value
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((respuesta) => {
         respuesta.json().then((data) => {
-            console.log(data);
-            const nombre = data.name;
-            const peso = data.weight;
-            const altura = data.height;
-            const tipo = data.types[0].type.name;
-            const imagen = data.sprites.front_default;
+            console.log(data)
+            const nombre = data.name
+            const peso = data.weight
+            const altura = data.height
+            const tipo = data.types[0].type.name
+            const imagen = data.sprites.front_default
 
-            pokemonNombre.innerHTML = nombre;
+            pokemonNombre.innerHTML = nombre
 
             pokemonTabla.innerHTML = `
             <tr>
@@ -36,6 +36,15 @@ pokemonFormulario.addEventListener("submit", (event) => {
             pokemonImagen.innerHTML = `
             <img src="${imagen}" alt="${nombre}">
         `;
-        });
-    });
-});
+        })
+    })
+})
+
+let estadisticas = [] // variable arreglo vacio
+
+data.stats.forEach(function (s) { // parametero s para recibir valor
+    estadisticas.push({ // metodo push para agregar valores al array
+        label: s, stats.name, // label es igual al mapeo del objeto. se le pasa al parametro s el valor del objeto que está dentro de otro objeto
+        y: s.base_stat, // valor cuantitativo del eje y, se pasa al parametro el mapeo del grafico base_stat 
+    })
+})
